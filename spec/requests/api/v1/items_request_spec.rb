e15cc5084b3bc823c9d80075648a435b1d19127e
Mad Hallé <mad.halle@icloud.com>
2020-07-13 17:04:14 -0600

    item destroy method working

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -48,4 +48,17 @@ describe "Items API" do
     expect(item.name).to_not eq(previous_item_name)
     expect(item.name).to eq("oogabooga")
   end
+
+  it "can destroy an item" do
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error{ActiveRecord::RecordNotFound}
+
+  end
 end
