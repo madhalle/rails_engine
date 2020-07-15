0ea7dfc2b675bef757a8f3bdcd54f69d9c6529c0
Mad Hallé <mad.halle@icloud.com>
2020-07-14 18:42:16 -0600

    merchant items response working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :show, :create, :update, :destroy]
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
+        resources :items, only: [:index], controller: "merchant_items"
+      end
     end
   end
 end
