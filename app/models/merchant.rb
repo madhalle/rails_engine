cb048318b021c352ee1f1ee1ed3f800f3daae47a
Mad Hallé <mad.halle@icloud.com>
2020-07-16 07:20:03 -0600

    working through bi

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
+  has_many :invoice_items, through: :invoices
 
   def self.attribute_finder(query_params)
     attributes = query_params.keys
@@ -16,5 +17,12 @@ class Merchant < ApplicationRecord
     attribute = query_params.keys.first
     value = query_params[attribute]
     Merchant.where("merchants.#{attribute} ILIKE ?", "%#{value}%")
+  end
+
+  def self.most_revenue(merchant_quantity)
+    merchant_limit = merchant_quantity.values.first.to_i
+    # Merchant.joins(:invoices).joins(:transactions).where(transactions: {result: "success"})
+     merchants = Merchant.joins(invoices: [:invoice_items, :transactions]).where("transactions.result = 'success'").group("merchants.name").select("merchants.name, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+     require "pry"; binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
+        get '/most_revenue', to: 'business_intel#index'
       end
       resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         # get '/find', to: 'search#show'
